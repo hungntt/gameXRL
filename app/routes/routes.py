c63b33b7ade0e153ddd83c7462b1d84fdbe49335
hungntt <minatosnow@gmail.com>
2021-10-08 11:06:38 +0700

    Add error redirect page

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -192,6 +192,18 @@ def show_statistics():
                            minigrid_commented_obs_percents=minigrid_commented_obs_percents)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def server_error(e):
+    # note that we set the 404 status explicitly
+    return render_template('500.html'), 500
+
+
 if __name__ == '__main__':
     args_parser = main_parser()
     pong_api = API(cnx_type=args_parser.cnx_type, db='xrl')
